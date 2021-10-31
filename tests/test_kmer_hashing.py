61ac77a4fe0cbcfaabb427c8cec634680f2d496e
Ivar Grytten <ivar.grytten@gmail.com>
2021-10-31 16:59:29 +0100

    Div cleanup tests

--- tests/test_kmer_hashing.py
+++ tests/test_kmer_hashing.py
@@ -1,4 +1,4 @@
-from graph_kmer_index import sequence_to_kmer_hash, letter_sequence_to_numeric
+from graph_kmer_index import sequence_to_kmer_hash, letter_sequence_to_numeric, kmer_hash_to_sequence
 import numpy as np
 
 
@@ -11,15 +11,6 @@ def test():
     hash2 = sequence_to_kmer_hash(seq2)
     hash3 = sequence_to_kmer_hash(seq3)
     assert sequence_to_kmer_hash(seq1) == np.sum(np.power(4, np.arange(0, 31)[::-1]) * letter_sequence_to_numeric(seq1))
-
-    print(letter_sequence_to_numeric(seq1))
-    print(letter_sequence_to_numeric(seq2))
-    print(letter_sequence_to_numeric(seq3))
-    print(len(seq1))
-    print("hash 3:", hash3)
-    print("hash 2:", hash2)
-    print("hash 1:", hash1)
-    print(hash2 * 4)
     assert hash1 != hash2
     assert hash1 == hash2 * 4 + 2
 
@@ -28,3 +19,20 @@ def test():
     print(sequence_to_kmer_hash("G"*31))
     assert sequence_to_kmer_hash("G"*31) == 4611686018427387903
 
+
+def test_hash_and_reverse():
+    sequence = "atg"
+    hash = sequence_to_kmer_hash(sequence)
+    print(hash)
+    sequence2 = kmer_hash_to_sequence(hash, len(sequence))
+    print(sequence2)
+
+    sequence = "ggtagctctcgccagctcctagaaggagga"
+    hash = sequence_to_kmer_hash(sequence)
+    print(hash)
+    sequence2 = kmer_hash_to_sequence(hash, len(sequence))
+    print(sequence, sequence2, hash, sequence_to_kmer_hash(sequence2))
+
+
+    # todo:
+    assert sequence2 == sequence, "%s != %s" % (sequence2, sequence)
