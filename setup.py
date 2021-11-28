374bba1fc1b3e582daef35a95c61705d5c928799
Ivar Grytten <ivar.grytten@gmail.com>
2021-11-28 16:26:17 +0100

    Setup cleanup

--- setup.py
+++ setup.py
@@ -1,24 +1,6 @@
-from setuptools import setup
 from distutils.core import setup, Extension
-#from Cython.Build import cythonize
-#from distutils.extension import Extension
-#from Cython.Distutils import build_ext
 
-"""
-ext_modules=[
-    Extension("graph_kmer_index.cython_kmer_index",
-              ["graph_kmer_index/cython_kmer_index.pyx"],
-              libraries=["m"],
-              extra_compile_args = ["-O3", "-ffast-math", "-march=native", "-fopenmp" ],
-              extra_link_args=['-fopenmp']
-              ),
-    Extension("graph_kmer_index.cython_reference_kmer_index",
-              ["graph_kmer_index/cython_reference_kmer_index.pyx"],
-              libraries=["m"],
-              extra_compile_args = ["-O3", "-ffast-math", "-march=native", "-fopenmp" ],
-              extra_link_args=['-fopenmp']
-              )]
-"""
+
 
 setup(name='graph_kmer_index',
       version='0.0.1',
@@ -36,6 +18,4 @@ setup(name='graph_kmer_index',
       entry_points={
             'console_scripts': ['graph_kmer_index=graph_kmer_index.command_line_interface:main']
       },
-      #cmdclass = {"build_ext": build_ext},
-      #ext_modules = ext_modules
 )
